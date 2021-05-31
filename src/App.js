dbe9566cb18b7cc9f482d54690b8924e21a9c707
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-31 14:32:48 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <p>Authentication App</p>
+    </div>
+  )
+}
+
+export default App
